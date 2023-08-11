9487beb03813d842d6b39baebf5cb7d6af032c8c
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-11 00:26:12 +0000

    Add newline character

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,5 +3,5 @@ import sys
 
 status_code = 1
 
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(status_code)
