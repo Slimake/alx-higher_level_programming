0eb69d56f0f4659d8d8a0d430ab011e764c7fcb7
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-14 11:04:01 +0000

    Refactor 4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
 
     for name in dir(hidden_4):
-        if name[0] != "_" and name[1] != "_":
-            print(name)
+        if not name.startswith("__"):
+            print("{}".format(name))
