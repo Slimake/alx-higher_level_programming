d949fbbe9b5ed35ebff1ca6f9da2742b216ad16b
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-17 02:25:07 +0000

    Update 0-square_matrix_simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import copy
 
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = copy.deepcopy(matrix)
-    for row in new_matrix:
-        for i in range(len(row)):
-            row[i] = row[i] * row[i]
-    return new_matrix
+    if matrix is not None:
+        new_matrix = []
+        for row in matrix:
+            new_matrix.append(list(map(lambda x: x**2, row)))
+        return new_matrix
+    return None
