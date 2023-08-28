057bc6ead3a75fb09cbfeb9f246a3e67619b1c67
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-28 21:11:44 +0000

    0-safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    new_list = ""
+    count = 0
+
+    for i in my_list:
+        count += 1
+
+    if x < count:
+        count = x
+
+    for i in range(1, count + 1):
+        new_list = new_list + str(i)
+        
+    print(new_list)
+
+    return my_list[count - 1]
