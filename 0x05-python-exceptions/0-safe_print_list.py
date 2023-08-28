0b23c2cc44402a3aaa27c635e25e913a1d51c05a
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-28 22:22:29 +0000

    More Code refactor

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,8 @@ def safe_print_list(my_list=[], x=0):
     while index < x:
         try:
             print("{}".format(my_list[index]), end="")
-        except IndexError:
+        except Exception:
             break
         index += 1
     print()
     return index
-    
