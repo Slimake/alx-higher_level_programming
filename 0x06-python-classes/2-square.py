fdd67e7c73c4fcac269c62bf29e2c3d437aa9b84
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-31 01:49:40 +0000

    2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+2-square module
+Declares class sqaure with private attribute size, also raises an exception
+"""
+
+
+class Square:
+    """
+    class definition
+    
+    Args:
+        size: size of the square
+    """
+    def __init__(self, size=0):
+        """
+        The __init__ method defines a data attribute and
+        raises an exception if size is not an integer
+
+        Attributes:
+            size: size of the square, default value of 0 if no size passed 
+        """
+
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size= size
