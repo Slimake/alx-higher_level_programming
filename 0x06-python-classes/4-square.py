7a82aee0edf97a0dba76456dcb17572d1fecbc84
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-31 03:24:54 +0000

    4-square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+4-square module
+Declares class sqaure with private attribute size
+using getter and setter, also raises an exception
+"""
+
+
+class Square:
+    """
+    class definition
+
+    Args:
+        size (int): size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        The __init__ method defines a data attribute and
+        raises an exception if size is not an integer.
+
+        Attributes:
+            size (int): size of the square,
+            default value of 0 if no size passed.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Returns current square area
+
+        Returns:
+            int: area
+        """
+        return (self.size * self.size)
