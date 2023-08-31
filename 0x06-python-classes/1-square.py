ddabbeaa93725de15c136262ec817d4d670caaab
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-31 00:58:16 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """A square class with an init method"""
+    def __init__(self, size):
+        """An init method that defines a private data attribute"""
+        self.__size = size
