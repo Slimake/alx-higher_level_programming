67876e9e4470a88ea19a09405c7335d7eaf7f452
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-08-31 00:42:24 +0000

    modify doc string

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    """A square class that defines a class.
-Defines an __init__ method.
     """
-    def __init__(self) -> None:
-        """This is called during the creation of an object.
+    A square class that defines a class.
+    Defines an __init__ method.
+    """
+    def __init__(self):
+        """
+        This is called during the creation of an object.
         The pass is used to indicate the class does nothing.
         """
         pass
