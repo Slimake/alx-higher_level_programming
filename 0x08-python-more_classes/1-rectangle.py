667b4c5104e9dd24cd9ed573619693b1cd86f5ee
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-04 17:15:11 +0000

    Update 1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,23 +7,26 @@ Defines a Rectangle class
 
 
 class Rectangle:
-    def __init__(self, width=0, height=0):
-        """An init method that defines a private data attribute.
+    """
+    Defines class rectangle with private attribute width and height
 
-        Args:
-            width (int): width of the rectangle
-            height (int): height of the rectangle
-        """
-        self.__width = width
-        self.__height = height
+    Args:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """Initialise a rectangle"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """:int: value."""
+        """Getter gets width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter sets width"""
         if type(value) is not int:
             raise ValueError("widht must be >= 0")
         elif value < 0:
@@ -33,11 +36,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """:int: value."""
+        """Getter gets width"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter sets width"""
         if type(value) is not int:
             raise ValueError("height must be >= 0")
         elif value < 0:
