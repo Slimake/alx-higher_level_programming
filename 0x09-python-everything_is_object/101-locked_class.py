752b82ee5b1edf4620ab37a56099160850f223fd
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-07 18:26:57 +0000

    Add doc string

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+101-locked_class module
+Defines a LockedClass
+"""
+
+
 class LockedClass(object):
+    """
+    Defines LockedClass
+
+    A locked class that uses __slots__ to prevent users from /
+    dynamically create new instance attributes
+    """
     __slots__ = ['first_name']
