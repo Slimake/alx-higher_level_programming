58820f1c4f5b38bdedd2df04392a4db573cdec14
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-13 10:23:18 +0000

    Add 0-read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0-read_file module
+
+Reads a text file and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads and print a text file to the stdout
+    """
+    a_file = open(filename, encoding="utf-8")
+    print(a_file.read())
