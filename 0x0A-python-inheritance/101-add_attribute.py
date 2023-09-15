8a7af4385ac06b996707bbb8a20e9de20a19a5bd
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-15 11:32:23 +0000

    101-add_attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+100-add_attribute module
+
+Contains a function that adds new attribute to an object if possible
+"""
+
+
+def add_attribute(obj, key, value):
+    """Adds a new attribute to an object"""
+    if '__dict__' in dir(obj):
+        obj.__setattr__(key, value)
+    else:
+        raise TypeError("Can't add new attribute")
