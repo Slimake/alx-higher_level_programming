45978eec4da3cc3db3c49c717b92f0636f6017a8
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-16 21:15:44 +0000

    Add rectangle file

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+rectangle module
+
+Contains Rectangle class
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Define Rectangle class that inherit from Base class
+
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the class instance
+
+        Args:
+            width (int): the width of the rectangle
+            height (int): the height of the rectangle
+            x (int): x coordinate
+            y (int): y coordinate
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        self.__y = value
