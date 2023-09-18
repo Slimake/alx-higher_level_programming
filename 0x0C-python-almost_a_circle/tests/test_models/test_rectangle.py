2edfea49634059c45b268e22fc22155bb16ac4f0
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-18 22:17:10 +0000

    Update test_rectangle.py file

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,10 +10,40 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(Rectangle(10, 2, 0, 0, 12).id, 12)
         self.assertEqual(Rectangle(1, 3, 2, 0, 5).x, 2)
         self.assertEqual(Rectangle(3, 3, 0, 4, 5).y, 4)
-        self.assertEqual(Rectangle(-1, -3, 0, 0, 5).width, -1)
-        self.assertEqual(Rectangle(-1, -3, 0, 0, 5).height, -3)
-        self.assertEqual(Rectangle(-1, -3, 0, 0, 5).height, -3)
-        self.assertEqual(Rectangle(float("inf"), float("NaN"),
-                                   0, 0, 2).width, float("inf"))
-        self.assertNotEqual(Rectangle(4, float("NaN"),
-                                      0, 0, 2).height, float("NaN"))
+        self.assertEqual(Rectangle(2, 3, 0, 0, 5).width, 2)
+        self.assertEqual(Rectangle(1, 3, 0, 0, 5).height, 3)
+        self.assertEqual(Rectangle(3, 2).area(), 6)
+        self.assertEqual(Rectangle(8, 7, 0, 0, 12).area(), 56)
+
+        with self.assertRaises(TypeError):
+            Rectangle("2", 3, 0, 0, 5).width
+
+        with self.assertRaises(TypeError):
+            Rectangle(2, "3", 0, 0, 5).height
+
+        with self.assertRaises(TypeError):
+            Rectangle(2, 3, "4", 0, 5).x
+
+        with self.assertRaises(TypeError):
+            Rectangle(2, 3, 4, "5", 5).y
+
+        with self.assertRaises(TypeError):
+            Rectangle(float("inf"), 3, 0, 0, 2).width
+
+        with self.assertRaises(TypeError):
+            Rectangle(2, float("NaN"), 0, 0, 2).height
+
+        with self.assertRaises(ValueError):
+            Rectangle(-2, 3, 0, 0, 5).width
+
+        with self.assertRaises(ValueError):
+            Rectangle(2, 0, 0, 0, 5).height
+
+        with self.assertRaises(ValueError):
+            Rectangle(2, 3, -4, 0, 5).x
+
+        with self.assertRaises(ValueError):
+            Rectangle(2, 3, 4, -5, 5).y
+
+        with self.assertRaises(ValueError):
+            Rectangle(-2, 3).area
