ca5ea1d1bd059a4b8285e4ae98ee3d47fe453f63
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-19 13:01:54 +0000

    Add save_to_file, from_json_string function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,19 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes json string representation of list_objs to file"""
+        obj = []
+
+        if list_objs is not None:
+            for o in list_objs:
+                obj.append(cls.to_dictionary(o))
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(cls.to_json_string(obj))
