a867d611ce67d5a5594f9fcaa304e5d7b886d761
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-10-22 15:51:31 +0000

    Add IF NOT EXISTS

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- and the user user_0d_2.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_p
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_p
 wd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
