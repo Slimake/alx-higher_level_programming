14d42b23a76ff72d0e4cc97afe4a9b07112e5206
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-11-14 00:34:50 +0000

    Remove 'require()'

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-
 // Checks if command-line arguments found or not.
-if (argv.length === 2) {
+if (process.argv.length <= 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
-} else if (argv.length > 3) {
+} else {
   console.log('Arguments found');
 }
