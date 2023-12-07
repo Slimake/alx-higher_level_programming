d43b9108c46ee5bd94e65a4c0fc89f293e9f313f
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-12-07 02:55:36 +0000

    Add HTTP Basic Auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,15 +9,10 @@ and uses the GitHub API to display your id
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
     username = argv[1]
     token = argv[2]
 
-    url = "https://api.github.com/users/{}".format(username)
-    headers = {'Authorization': token}
-    try:
-        r = requests.get(url, headers=headers)
-        print(r.json()['id'])
-    except KeyError:
-        print(None)
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(username, token))
+    print(r.json().get('id'))
