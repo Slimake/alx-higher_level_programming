f08c750a057cc88bd9969cd0a0fb0a06c277fc51
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-01-05 15:06:13 +0000

    Modify file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 
 // Import the process.argv and fs module
-const { argv } = require('node:process');
-const fs = require('node:fs');
+const process = require('process');
+const fs = require('fs');
+
+const filepath = process.argv[2];
 
 // Read and priint file content or print error message
-fs.readFile(argv[2], 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+fs.readFile(filepath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
