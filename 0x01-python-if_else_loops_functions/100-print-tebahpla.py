d30bd8b9d2a913bc1ca83093f7ad081f77cca80f
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-12 21:01:36 +0000

    100-print_tebahpla

---
+++ 0x01-python-if_else_loops_functions/100-print-tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(ord('z'), ord('`'), -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{0}".format(chr(i).upper()), end="")
