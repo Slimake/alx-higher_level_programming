db71607efdbfd8fadaff78963481ad1e4cc66b8b
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-12 22:39:54 +0000

    Refactor code

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    str_copy = str[:]
-    result = ""
-    for index, char in enumerate(str_copy):
-        if not index == n:
-            result += char
-    return result
+    if n >= 0:
+        str = str[0:n] + str[n+1:]
+    return str
