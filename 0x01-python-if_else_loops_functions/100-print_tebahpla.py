db2df83e76425d05db9bee75d4cf536d7abef230
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-12 21:06:39 +0000

    Refactor code

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('`'), -1):
     if i % 2 == 0:
-        print("{}".format(chr(i)), end="")
+        print("{:c}".format(chr(i)), end="")
     else:
-        print("{0}".format(chr(i).upper()), end="")
+        print("{:c}".format(chr(i).upper()), end="")
