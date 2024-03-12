85fd071696087c377f3883710e06fa69754681a7
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-12 21:08:58 +0000

    Modify code

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('`'), -1):
     if i % 2 == 0:
-        print("{:c}".format(chr(i)), end="")
+        print("{}".format(chr(i)), end="")
     else:
-        print("{:c}".format(chr(i).upper()), end="")
+        print("{}".format(chr(i).upper()), end="")
