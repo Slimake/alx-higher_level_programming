9423b3fbf56f79668062a1cc858d0b50a4c0b891
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-15 15:09:35 +0000

    Print a list of integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    """ Prints all integers of a list
+
+    Args:
+        my_list: a list
+
+    Returns:
+        None
+    """
+    # Cycle through my_list
+    for value in my_list:
+        # Print each value per line
+        print("{0:d}".format(value))
