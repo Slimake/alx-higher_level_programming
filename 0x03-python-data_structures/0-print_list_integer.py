b4717641a42ffbe944793df6de06bbc4a29ad09a
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-16 10:01:43 +0000

    Remove comment

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,7 +3,5 @@
 
 def print_list_integer(my_list=[]):
     if my_list:
-        # Cycle through my_list
         for value in my_list:
-            # Print each value per line
-            print("{0}".format(value))
+            print("{0:d}".format(value))
