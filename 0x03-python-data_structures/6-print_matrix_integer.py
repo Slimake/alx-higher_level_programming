42469f62ab0e740495c03c56d6c76d147570a6e3
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-16 19:05:55 +0000

    Prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers
+
+        Args:
+            matrix: a list
+        Returns:
+            The return value. None
+        """
+    # Check if matrix is empty
+    if matrix:
+        # Cycle through the row of matrix list
+        for lst in matrix:
+            # Cycle through the column of each inner list of matrix list
+            for index, num in enumerate(lst):
+                # print each element
+                if not (index == len(lst) - 1):
+                    print("{:d}".format(num), end=" ")
+                else:
+                    print("{:d}".format(num))
