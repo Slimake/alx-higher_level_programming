ebd3aee2901b5453885413f1d24afefb9bdbf4a7
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-16 10:08:59 +0000

    Add doc string

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,17 @@
 
 
 def print_list_integer(my_list=[]):
+    """ Prints all integers of a list
+
+    Args:
+        my_list: a list
+
+    Returns:
+        None
+    """
+
     if len(my_list):
+        # Cycle through my_list
         for num in my_list:
+            # Print each value per line
             print("{:d}".format(num))
