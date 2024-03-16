6f4f7e48b2962d0a59a714a2c13f7e037e301c41
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-16 22:31:19 +0000

    Remove the for loop

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -8,16 +8,12 @@ def delete_at(my_list=[], idx=0):
         Returns:
             The return value. list
         """
-    # If list is empty
+    # If list is not empty
     if my_list:
         # if idx is negative or out of range, return my_list
         if idx < 0 or idx > len(my_list) - 1:
             return my_list
-        # Cycle through my_list
-        for i in my_list:
-            # When idx equals index in my_list
-            if idx == i:
-                # Delete item
-                del my_list[idx]
+        # Delete item
+        del my_list[idx]
         # return list
         return my_list
