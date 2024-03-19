cd72b51c8bf511e981fb997cf9c5d394cca923c3
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-19 14:57:49 +0000

    Check if cycle is present in the node

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Check if a singly linked list has a cycle in it.
+ * @list: pointer to the head of the list
+ * Return: 0, if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *current;
+    
+    current = list;
+
+    while (1)
+    {
+        current = current->next;
+
+        if (current == NULL)
+            return (0);
+        else if (current == list)
+            return (1);
+    }
+}
