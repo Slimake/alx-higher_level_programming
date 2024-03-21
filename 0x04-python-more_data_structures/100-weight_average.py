94f1105203572578656f301c1f19f6c1042cf329
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-03-21 00:36:25 +0000

    Weighted Average of all integers tuple

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """Returns the weighted average of all integers tuple"""
+    if my_list:
+        tuple_sum = 0
+        last_item = 0
+        # Cycle through my_list
+        for row in my_list:
+            # Sum and store the tuple element in my_list
+            tuple_sum += row[0] * row[1]
+            # Store the last item in the tuple
+            last_item += row[-1]
+        # Divide the stored sum and the last item
+        # of each tuple in my_list and return
+        print(tuple_sum, last_item)
+        return tuple_sum / last_item
+    else:
+        return 0
