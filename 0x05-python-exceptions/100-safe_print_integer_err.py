0d2c342f44d6d35715cbb6e7faae9cdbdf3cc7e0
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-02 22:17:13 +0000

    Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as exc:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(exc.__str__())
+        return False
