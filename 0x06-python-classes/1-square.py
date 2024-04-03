d1ff80e3ac6765318a709cf94122f9328f5f4b74
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-03 20:37:15 +0000

    Define a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains Square class that defines a square"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """Initializes the instance of Square class"""
+        self.__size = size
