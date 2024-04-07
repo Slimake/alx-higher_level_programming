4d5755fa5d571d80a735e6f275252ab79a23b968
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-07 20:06:09 +0000

    Add two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 0-add_integer contains a function that adds two integers
+
+Returns the addition of the two integers
+"""
+
+def add_integer(a, b=98):
+    """
+        Returns the addition of two integers
+    """
+    if (not type(a) in (float, int)):
+        raise TypeError("a must be an integer")
+    if (not type(b) in (float, int)):
+        raise TypeError("b must be an integer")
+    return int(a + b)
