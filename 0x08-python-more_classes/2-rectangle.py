ec2298cde43fc7b3dc506474cf41f0819070e223
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-08 19:35:17 +0000

    Refactor code

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -52,10 +52,12 @@ class Rectangle:
 
     def area(self):
         """Return the area of a rectangle"""
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
-        """Return the perimeter of a rectangle"""
-        if self.width == 0 or self.height == 0:
+        """Return 2 * (self.__width + self.__height),
+        Else if width or height equals 0, return 0
+        """
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
