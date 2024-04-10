5961e6ebd1528257a5a65aa569ecd4ed27bb6fd9
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-10 00:01:45 +0000

    101-locked_class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+Module 101-locked class
+
+Defines LockedClass
+"""
+
+
 class LockedClass:
+    """
+    Defines a class that prevents a user from dynamically create new instance
+    attributes.
+    """
     __slots__ = ["first_name"]
