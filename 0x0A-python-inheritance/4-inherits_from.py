ea111ec15ed2cbd1179143fda4270b909f091889
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-15 22:38:25 +0000

    True if Obj is instance of class or False if not

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+"""
+Module 4-inherits_from.py
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class
+    that inherits (directly or indirectly) from the specified class
+    otherwise False
+    """
+    return type(obj) is not a_class and issubclass(obj.__class__, int)
