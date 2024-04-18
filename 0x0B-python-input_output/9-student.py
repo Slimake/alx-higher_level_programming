9cca83655d6936bb90e4d6a2f0db9da8a335ffff
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-18 23:41:49 +0000

    9-student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
-"""
-9-student module
-
-Contains Student class
+""" Student class module
 """
 
 
 class Student:
-    """This class defines a Student class.
-
-    It defines the init method and the to_json method.
+    """ Student class
     """
     def __init__(self, first_name, last_name, age):
-        """This initializes the object instance.
-
-        Args:
-            first_name (str): first name of the student.
-            last_name (str): last name of the student.
-            age (int): age of the student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """This returns the public instance dictionary.
-
-        Returns:
-            self.__dict__
-        """
         return self.__dict__
