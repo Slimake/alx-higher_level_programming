f9cb1d9ee5338fa6e385ca24ac1c558fddb4f7cc
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-18 09:59:12 +0000

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,9 @@ import os
 
 
 def write_file(filename="", text=""):
+    """
+    Writes a string to a text file
+    Returns the number of characters written
+    """
     with open(filename, "w", encoding="utf-8") as my_file:
         return my_file.write(text)
