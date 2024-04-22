aea512c4eadc1b9cb5cd6960f824af4e6519c2b0
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-22 23:04:26 +0000

    Unittest for models

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Test base
+"""
+import unittest
+from models.base import Base
+
+class TestBaseClass(unittest.TestCase):
+    """ Test for Base class
+    """
+    def test_id_assert_equal(self):
+        self.assertEqual(1, Base().id)
+        self.assertEqual(12, Base(12).id)
+        self.assertEqual(2, Base().id)
+        self.assertEqual(int(float(3.4)), Base(3).id)
+
+    def test_id_assert_true(self):
+        self.assertTrue(1, Base().id)
+        self.assertTrue(12, Base(12).id)
+        self.assertTrue(2, Base().id)
+        self.assertTrue(int(float(3.4)), Base(3).id)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+""" test_rectangle module
+"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangleClass(unittest.TestCase):
+    """ Test for Rectangle class
+    """
+    def test_id_assert_equal(self):
+        self.assertTrue(1, Rectangle(10, 5, 0, 0).id)
+        self.assertTrue(4, Rectangle(10, 5, 0, 0, 4).id)
+
+    def test_width_assert_equal(self):
+        self.assertEqual(10, Rectangle(10, 5).width)
+
+    def test_width_assert_true(self):
+        self.assertTrue(10, Rectangle(10, 5).width)
+
+    def test_width_assert_raises_type(self):
+        with self.assertRaises(TypeError):
+            Rectangle("10", 5, 0, 0, 4)
+        
+    def test_width_assert_raises_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(0, 5)
+
+    def test_height_assert_equal(self):
+        self.assertEqual(5, Rectangle(10, 5).height)
+
+    def test_height_assert_true(self):
+        self.assertTrue(5, Rectangle(10, 5).height)
+
+    def test_height_assert_raises_type(self):
+        with self.assertRaises(TypeError):
+            Rectangle(10, "5", 0, 0, 4)
+
+    def test_height_assert_raises_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(10, 0)
+
+    def test_x_assert_equal(self):
+        self.assertEqual(0, Rectangle(10, 5).x)
+
+    def test_x_assert_true(self):
+        self.assertTrue(1, Rectangle(10, 5, 1, 1).x)
+
+    def test_x_assert_false(self):
+        self.assertFalse(0, Rectangle(10, 5).x)
+
+    def test_x_assert_raises_type(self):
+        with self.assertRaises(TypeError):
+            Rectangle(10, 5, "1", 0, 4).x
+
+    def test_x_assert_raises_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(10, 5, -1)
+
+    def test_y_assert_equal(self):
+        self.assertEqual(0, Rectangle(10, 5).y)
+
+    def test_y_assert_true(self):
+        self.assertTrue(1, Rectangle(10, 5, 1, 1).y)
+
+    def test_y_assert_false(self):
+        self.assertFalse(0, Rectangle(10, 5).y)
+
+    def test_y_assert_raises_type(self):
+        with self.assertRaises(TypeError):
+            Rectangle(10, 5, 1, "1", 4).y
+
+    def test_y_assert_raises_value(self):
+        with self.assertRaises(ValueError):
+            Rectangle(10, 5, 0, -1)
+
+if __name__ == "__main__":
+    unittest.main()
