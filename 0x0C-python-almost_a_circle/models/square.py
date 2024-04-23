d1ea2991df3e50ee3bba6146d66b008ec551bdc8
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-23 17:13:05 +0000

    Add public getter and setter size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,19 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ Getter for size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter for size
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ String representation of Square class
         """
