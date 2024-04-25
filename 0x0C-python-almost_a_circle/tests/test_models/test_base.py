544da8c5bc53d8cdf30beb60497b9649d713a498
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-04-25 13:02:00 +0000

    Add test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,24 +11,25 @@ class TestBaseClass(unittest.TestCase):
     """
     @classmethod
     def setUpClass(self):
-        self.rect = Rectangle(10, 7, 2, 8).to_dictionary()
+        self.base = Base()
+        self.rect = Rectangle(10, 7, 2, 8)
+        self.to_json = Base.to_json_string([self.rect.to_dictionary()])
 
     def test_id_assert_equal(self):
-        self.assertEqual(2, Base().id)
+        self.assertEqual(1, self.base.id)
         self.assertEqual(12, Base(12).id)
-        self.assertEqual(3, Base().id)
+        self.assertEqual(1, self.base.id)
         self.assertEqual(int(float(3.4)), Base(3).id)
 
-    def test_id_assert_true(self):
-        self.assertTrue(2, Base().id)
-        self.assertTrue(12, Base(12).id)
-        self.assertTrue(3, Base().id)
-        self.assertTrue(int(float(3.4)), Base(3).id)
+    def test_to_json_string_assert_equal(self):
+        self.assertEqual(
+                '[{"id": 2, "width": 10, "height": 7, "x": 2, "y": 8}]',
+                self.to_json)
 
-    def test_to_json_string_assert_true(self):
-        self.assertTrue(
-                [{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}],
-                Base.to_json_string([self.rect]))
+    def test_from_json_string_assert_equal(self):
+        self.assertEqual(
+                [{"x": 2, "width": 10, "id": 2, "height": 7, "y": 8}],
+                Base.from_json_string(self.to_json))
 
 
 if __name__ == "__main__":
