2d0c27be48f60effba65722c1843bdc877b3b9b1
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-05-23 12:50:06 +0000

    Modify code

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,8 @@
+-- url = https://s3.amazonaws.com/intranet-projects-files/holbertonschool-hi-- gher-level_programming+/274/hbtn_0d_tvshows.sql
+-- Import the database dump from url into your MySQL server.
 
-SELECT title, genre_id 
-FROM tv_shows, tv_show_genres 
-WHERE tv_show_genres.show_id = tv_shows.id 
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT title, genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
