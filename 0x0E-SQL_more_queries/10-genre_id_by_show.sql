4d8cb15536684f50c2bfe385d9b74528cb33f4e7
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-05-23 12:27:12 +0000

    Refactor the sql command

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,4 @@
 
-
-CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
-USE hbtn_0d_tvshows;
-SOURCE hbtn_0d_tvshows;
 SELECT title, genre_id 
 FROM tv_shows, tv_show_genres 
 WHERE tv_show_genres.show_id = tv_shows.id 
