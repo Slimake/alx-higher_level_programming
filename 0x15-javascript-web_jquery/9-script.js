7ea3481afee434c8fd3bdbb1dd4e7c1cc0dabfb3
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-08-08 01:25:34 +0100

    Fetch from URL and display the value of hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+const $ = window.$;
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+
+$(document).ready(function () {
+  $.get(url, function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('DIV#hello').text(data.hello);
+    }
+  });
+});
